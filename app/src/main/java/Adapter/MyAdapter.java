5312e73bd056a1e5bd26c992a69120cf473bcfd4
Ekaterina <ek.stupina@gmai.com>
2018-04-27 17:41:00 +0300

    Populated MyAdapter constructor and inflated list_item.xml

--- app/src/main/java/Adapter/MyAdapter.java
+++ app/src/main/java/Adapter/MyAdapter.java
@@ -3,8 +3,11 @@ package Adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.tulit.tulitimageviewer.R;
 
 import java.util.List;
 
@@ -15,14 +18,20 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
     private Context context;
     private List<ListItem> listItems;
 
-    public MyAdapter(Context context, List listitem) {
+    public MyAdapter(Context context, List<ListItem> listItems) {
+        this.context = context;
+        this.listItems = listItems;
 
     }
 
+    // Inflates our list_item.xml file
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item, parent, false);
+
+        return new ViewHolder(view);
     }
 
     @Override
