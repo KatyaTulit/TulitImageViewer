527457ad0b7a185af2756f5cc41b248599e34f9c
Ekaterina <ek.stupina@gmai.com>
2018-04-27 17:30:51 +0300

    Created Adapter for RecyclerView and outlined all methods

---
+++ app/src/main/java/Adapter/MyAdapter.java
@@ -0,0 +1,45 @@
+package Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import Model.ListItem;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
+
+    private Context context;
+    private List<ListItem> listItems;
+
+    public MyAdapter(Context context, List listitem) {
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    // Holds all the items that we have in the list_item.xml
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
