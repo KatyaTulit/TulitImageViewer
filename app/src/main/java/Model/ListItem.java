e1b83e69aad4f70f7263025db849d192ffb43be2
Ekaterina <ek.stupina@gmai.com>
2018-04-27 17:04:41 +0300

    Created ListItem class file, list_item.xml with CardView

---
+++ app/src/main/java/Model/ListItem.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class ListItem {
+    private String name;
+
+    public ListItem(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
