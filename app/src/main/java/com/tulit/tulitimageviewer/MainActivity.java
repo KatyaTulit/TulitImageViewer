f7391c4b3cd6aac408f56f24da3fb1aa7f128ef9
Ekaterina <ek.stupina@gmai.com>
2018-04-27 18:14:34 +0300

    Assembled RecyclerView in MainActivity and instantiated adapter

--- app/src/main/java/com/tulit/tulitimageviewer/MainActivity.java
+++ app/src/main/java/com/tulit/tulitimageviewer/MainActivity.java
@@ -2,12 +2,41 @@ package com.tulit.tulitimageviewer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Adapter.MyAdapter;
+import Model.ListItem;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private List<ListItem> listItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerViewID);
+        // Every item will have fixed size (so that everything is aligned)
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+
+        listItems = new ArrayList<>();
+
+        // Generate some data
+        for (int i = 0; i < 20; i++) {
+            ListItem item = new ListItem("Item #" + i);
+            listItems.add(item);
+        }
+
+        adapter = new MyAdapter(this, listItems);
+        recyclerView.setAdapter(adapter);
+
     }
 }
