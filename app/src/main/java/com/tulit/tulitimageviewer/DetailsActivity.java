190a5b980114c0e77d22ad4941330973336dfeaa
katya <ek.stupina@gmail.com>
2018-05-06 13:53:34 +0300

    Added imageView to the details layout, load image

--- app/src/main/java/com/tulit/tulitimageviewer/DetailsActivity.java
+++ app/src/main/java/com/tulit/tulitimageviewer/DetailsActivity.java
@@ -2,10 +2,14 @@ package com.tulit.tulitimageviewer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class DetailsActivity extends AppCompatActivity {
     private TextView name;
+    private ImageView image;
     private Bundle extras;
 
     @Override
@@ -15,11 +19,16 @@ public class DetailsActivity extends AppCompatActivity {
 
         extras = getIntent().getExtras(); // get the extras that we pass with intent
 
-
         name = findViewById(R.id.dNameID);
+        image = findViewById(R.id.dImageID);
 
         if (extras != null) {
             name.setText(getString(R.string.dNameText, extras.getString("name")));
+            Picasso.get()
+                    .load(extras.getString("url"))
+                    .fit()
+                    .centerInside()
+                    .into(image);
         }
 
     }
